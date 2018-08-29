8deb650fdaf6b9c8b3f4b34917af48f81402f4d8
itsanti <kurov.aleksandr+github@gmail.com>
2018-08-29 22:03:26 +0300

    HW5: init

---
+++ src/hw5/exmpls/JThread.java
@@ -0,0 +1,24 @@
+package hw5.exmpls.;
+
+class JThread extends Thread {
+     
+    JThread(String name){
+        super(name);
+    }
+     
+    @Override
+    public void run(){
+         
+        System.out.printf("%s started... \n", Thread.currentThread().getName());
+        try{
+            for (int i = 0; i < 10; i++) {
+            System.out.println("Count in Jthread: " + i);
+            Thread.sleep(1000);
+            }
+        }
+        catch(InterruptedException e){
+            System.out.println("Thread has been interrupted");
+        }
+        System.out.printf("%s fiished... \n", Thread.currentThread().getName());
+    }
+}
---
+++ src/hw5/exmpls/Thread_1.java
@@ -0,0 +1,23 @@
+package hw5.exmpls.;
+
+
+public class Thread_1 {
+
+   
+    public static void main(String[] args) {
+    
+        
+    new NewThread(); // create a new thread
+
+    try {
+        for(int i = 5; i > 0; i--) {
+        System.out.println("Main Thread: " + i);
+        Thread.sleep(1000);
+      }
+    } catch (InterruptedException e) {
+      System.out.println("Main thread interrupted.");
+    }
+    System.out.println("Main thread exiting.");
+    }
+    
+}
