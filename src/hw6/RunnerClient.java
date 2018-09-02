beb23c2294aec90e781339a47405f753f786d19b
itsanti <kurov.aleksandr+github@gmail.com>
2018-09-03 00:02:07 +0300

    HW6: network. v1.0

---
+++ src/hw6/RunnerClient.java
@@ -0,0 +1,13 @@
+package hw6;
+
+/**
+ * Homework #6
+ *
+ * @author Aleksandr Kurov
+ * @version dated Август 30, 2018
+ */
+public class RunnerClient {
+    public static void main(String[] args) {
+        SChatClient.run();
+    }
+}
